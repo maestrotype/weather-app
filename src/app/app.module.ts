f81f41b72748e89fb24b7cce2b16fa8b1b8fc332
andrey dan <adan3891@gmail.com>
2019-11-11 23:08:09 +0200

    mat-icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -28,6 +29,7 @@ import { MatInputModule } from '@angular/material';
     MatSliderModule,
     MatButtonModule,
     MatGridListModule,
+    MatIconModule,
     MatInputModule,
     MatCardModule,
     MatToolbarModule
