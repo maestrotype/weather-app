174f8fe4403b0cfaccc240ff3e2107a185edcba0
andre-dan <andrey.danichkin@swanlogic.com>
2019-11-14 18:17:55 +0200

    geolocation

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -10,15 +10,17 @@ import { ILocation } from '../location';
 export class ContentComponent implements OnInit {
 
   location: ILocation;
+  data: any;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
     this.weatherService.getWeather().subscribe((response) => {
-      console.log(response);
+      this.data = response;
+      console.log(this.data);
       this.location = {
-        locationId: 2,
-        locationName: 'Tel Aviv',
+        locationId: this.data.AdministrativeArea.ID,
+        locationName: this.data.LocalizedName,
         locationWeather: '33 C'
       }
     })
