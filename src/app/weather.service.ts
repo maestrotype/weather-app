c19c3181cab0ee723326b0cdc98cf58f9427885c
andrey dan <adan3891@gmail.com>
2019-11-17 18:57:14 +0200

    search city

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,16 +9,23 @@ import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class WeatherService {
-  apiKey = '2wjh8FbS9yAwHBXpy9g9aGfItsTVKAYG';
-  url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=';
-  urlWeather = 'http://dataservice.accuweather.com/currentconditions/v1/215805?apikey=2wjh8FbS9yAwHBXpy9g9aGfItsTVKAYG';
-  // url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=2wjh8FbS9yAwHBXpy9g9aGfItsTVKAYG&q=49.949034%2C36.257655799999995';
+  locationKeyTay = '318819';
+  locationKeyTelAviv = '215805';
+  locationKeyTelKharkiv = '323903'
   latKharkov = '49.949034';
   lonKharkov = '36.257655799999995';
   latIsrael = '32.109333';
   lonIsrael = '34.855499';
+  latTay = '13.736717';
+  lonTay =  '100.523186';
   temperature = 0;
   res;
+  apiKey = '2wjh8FbS9yAwHBXpy9g9aGfItsTVKAYG';
+  url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=';
+  urlCitySearch = 'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=2wjh8FbS9yAwHBXpy9g9aGfItsTVKAYG&q=';
+  urlWeather = 'http://dataservice.accuweather.com/currentconditions/v1/' + this.locationKeyTay +'?apikey=2wjh8FbS9yAwHBXpy9g9aGfItsTVKAYG';
+  // url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=2wjh8FbS9yAwHBXpy9g9aGfItsTVKAYG&q=49.949034%2C36.257655799999995';
+  
   currentWeather = new BehaviorSubject<ILocation>({
     locationId: 0,
     city: 'Tel Aviv',
@@ -33,7 +40,7 @@ export class WeatherService {
     // `${this.latIsrael}%2C${this.lonIsrael}`).subscribe(data => (this.res = data));
     this.http.get(this.urlWeather)
       .subscribe(data => {
-        console.log('urlWeather', data[0].Temperature.Metric.Value);
+        console.log('urlWeather', data[0]);
         this.temperature = data[0].Temperature.Metric.Value
       });
        
@@ -56,18 +63,17 @@ export class WeatherService {
   }
 
   testCurrentWeather() {
-    console.log('test', this.res);
     return this.http
       .get<any>(
         this.url + this.apiKey + '&q=' +
-        `${this.latIsrael}%2C${this.lonIsrael}`
+        `${this.latKharkov}%2C${this.lonKharkov}`
       )
       .pipe(map(data => this.transformTest(data)))
   }
 
   private transformTest(data: any): ILocation {
     this.res = data;
-    console.log('test', this.res);
+    console.log('transformTest', this.res);
     return {
       locationId: 0,
       city: this.res.AdministrativeArea.LocalizedName,
@@ -82,31 +88,37 @@ export class WeatherService {
     search: string | number,
     country?: string
   ): Observable<ILocation> {
-    let uriParams = ''
-    if (typeof search === 'string') {
-      uriParams = `q=${search}`
-    } else {
-      uriParams = `zip=${search}`
-    }
+    let uriParams = '';
+    uriParams = `${search}`
+    // if (typeof search === 'string') {
+    //   uriParams = `q=${search}`
+    // } else {
+    //   uriParams = `zip=${search}`
+    // }
 
-    if (country) {
-      uriParams = `${uriParams},${country}`
-    }
+    // if (country) {
+    //   uriParams = `${uriParams},${country}`
+    // }
 
     return this.getCurrentWeatherHelper(uriParams)
   }
 
   private getCurrentWeatherHelper(uriParams: string): Observable<ILocation> {
+    console.log('getCurrentWeatherHelper', uriParams);
     return this.http
       .get<any>(
-        this.url
+        this.urlCitySearch + uriParams
         // `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
         //   `${uriParams}&appid=${environment.appId}`
       )
-      .pipe(map(data => this.transformToICurrentWeather(data)))
+      .pipe(map(data =>
+        this.transformToICurrentWeather(data)
+        ))
+        
   }
 
   private transformToICurrentWeather(data: any): ILocation {
+    console.log('search', data);
     return {
       locationId: data.id,
       city: data.name,
@@ -118,6 +130,7 @@ export class WeatherService {
   }
 
   updateCurrentWeather(search: string | number, country?: string) {
+    
     this.getCurrentWeather(search, country).subscribe(weather =>
       this.currentWeather.next(weather)
     )
